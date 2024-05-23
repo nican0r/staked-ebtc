151d04b211c2e7c36f10a6f401a28f801ec7680d
wtj2021 <wtj2022@proton.me>
2024-05-23 01:17:58 -0700

    adding more invariants

--- test/recon/Properties.sol
+++ test/recon/Properties.sol
@@ -6,11 +6,27 @@ import {Asserts} from "@chimera/Asserts.sol";
 import {Setup} from "./Setup.sol";
 
 abstract contract Properties is Setup, Asserts {
-    function crytic_total_balance_below_actual_balance() public {
+    function total_balance_below_actual_balance() public returns (bool) {
         t(mockEbtc.balanceOf(address(stakedEbtc)) >= stakedEbtc.totalBalance(), "actualBalance >= totalBalance");
     }
 
-    function crytic_total_assets_below_total_balance() public {
+    function total_assets_below_total_balance() public returns (bool) {
         t(stakedEbtc.totalBalance() >= stakedEbtc.totalAssets(), "totalBalance >= totalAssets");
+    }
+
+    function sum_of_user_balances_equals_total_supply() public returns (bool) {
+        uint256 sumOfUserBalances;
+        for (uint256 i; i < senders.length; i++) {
+            sumOfUserBalances += stakedEbtc.balanceOf(senders[i]);
+        }
+        t(sumOfUserBalances == stakedEbtc.totalSupply(), "sumOfUserBalances == totalSupply");
+    }
+
+    function sum_of_user_assets_equals_total_assets() public returns (bool) {
+        uint256 sumOfUserAssets;
+        for (uint256 i; i < senders.length; i++) {
+            sumOfUserAssets += stakedEbtc.convertToAssets(stakedEbtc.balanceOf(senders[i]));
+        }
+        t(sumOfUserAssets == stakedEbtc.totalAssets(), "sumOfUserAssets == totalAssets");
     }
 }
--- test/recon/TargetFunctions.sol
+++ test/recon/TargetFunctions.sol
@@ -5,28 +5,38 @@ pragma solidity ^0.8.0;
 import {BaseTargetFunctions} from "@chimera/BaseTargetFunctions.sol";
 import {BeforeAfter} from "./BeforeAfter.sol";
 import {Properties} from "./Properties.sol";
+import {vm} from "@chimera/Hevm.sol";
 
 abstract contract TargetFunctions is BaseTargetFunctions, Properties, BeforeAfter {
     uint256 public constant MAX_EBTC = 1e27;
+    address internal senderAddr;
 
     function deposit(uint256 amount) public {
-        amount = between(amount, 0, MAX_EBTC);
+        senderAddr = msg.sender;
+        amount = between(amount, 1, MAX_EBTC);
 
-        mockEbtc.mint(msg.sender, amount);
+        mockEbtc.mint(senderAddr, amount);
+
+        vm.prank(senderAddr);
         mockEbtc.approve(address(stakedEbtc), type(uint256).max);
 
-        try stakedEbtc.deposit(amount, msg.sender) {
+        vm.prank(senderAddr);
+        try stakedEbtc.deposit(amount, senderAddr) {
         } catch {
             t(false, "call shouldn't fail");
         }
     }
 
     function redeem(uint256 shares) public {
-        shares = between(shares, 0, stakedEbtc.balanceOf(msg.sender));
+        senderAddr = msg.sender;
+        shares = between(shares, 0, stakedEbtc.balanceOf(senderAddr));
 
-        try stakedEbtc.redeem(shares, msg.sender, msg.sender) {
+        vm.prank(senderAddr);
+        try stakedEbtc.redeem(shares, senderAddr, senderAddr) {
         } catch {
-            t(false, "call shouldn't fail");
+            if (stakedEbtc.previewRedeem(shares) > 0) {
+                t(false, "call shouldn't fail");
+            }
         }
     }
 }
